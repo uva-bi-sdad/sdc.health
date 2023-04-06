7498d48906c64a365a0d7b64430da669008f32c8
uva-bi-sdad <>
2023-04-06 18:20:07 +0000

    Update from https://github.com/uva-bi-sdad/sdc.health_dev/commit/4cf5ec4a099bfdb1009090f8d975b41d1255c346

--- Population Health/Health Opportunity Index/code/distribution/prepare_health_opportunity.R
+++ Population Health/Health Opportunity Index/code/distribution/prepare_health_opportunity.R
@@ -5,6 +5,7 @@ library(sf)
 hoi_2017 <- setDT(read_excel("Population Health/Health Opportunity Index/data/original/hoi.xlsx", sheet = 1))
 hoi_2022 <- setDT(read_excel("Population Health/Health Opportunity Index/data/original/hoi_indexes_quintile_2022.xlsx", sheet = 1))
 
+# 2017
 hoi_2017_sel <-
   hoi_2017[, .(
     geoid = Ctfips,
@@ -24,7 +25,15 @@ hoi_2017_sel[, value := as.integer(value)]
 
 hoi_2017_sel <- unique(hoi_2017_sel)
 
+# geographies
+geo_names <- fread("https://raw.githubusercontent.com/uva-bi-sdad/sdc.metadata/master/geographies.csv")
+geo_names <- geo_names[year == "2010", .(geoid, region_name, region_type)]
 
+hoi_2017_sel_mrg <- merge(hoi_2017_sel, geo_names, by = "geoid")
+hoi_2017_sel_mrg <- hoi_2017_sel_mrg[, .(geoid, measure, measure_type, region_name, region_type, value, year, moe)]
+
+
+# 2022
 hoi_2022_sel <- 
   hoi_2022[, .(
     geoid = CT, 
@@ -44,15 +53,18 @@ hoi_2022_sel[, value := as.integer(value)]
 
 hoi_2022_sel <- unique(hoi_2022_sel)
 
+# geographies
+geo_names <- fread("https://raw.githubusercontent.com/uva-bi-sdad/sdc.metadata/master/geographies.csv")
+geo_names <- geo_names[year == "2020", .(geoid, region_name, region_type)]
+
+hoi_2022_sel_mrg <- merge(hoi_2022_sel, geo_names, by = "geoid")
+hoi_2022_sel_mrg <- hoi_2022_sel_mrg[, .(geoid, measure, measure_type, region_name, region_type, value, year, moe)]
+
+
 # combine
 hoi_sel <- rbindlist(list(hoi_2017_sel, hoi_2022_sel))
 
-# geographies
-geo_names <- fread("https://raw.githubusercontent.com/uva-bi-sdad/sdc.metadata/master/geographies.csv")
-geo_names <- geo_names[year == "2010", .(geoid, region_name, region_type)]
 
-hoi_sel_mrg <- merge(hoi_sel, geo_names, by = "geoid")
-hoi_sel_mrg <- hoi_sel_mrg[, .(geoid, measure, measure_type, region_name, region_type, value, year, moe)]
 
 fwrite(hoi_sel_mrg, "Population Health/Health Opportunity Index/data/distribution/va_cttr_vdh_2017_2022_health_opportunity_profile.csv")
 
